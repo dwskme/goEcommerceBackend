db4b2ae28b4cedada2ecd71c7f7e896201bb5bfe
dwskme <officialsamirkc@gmail.com>
2024-03-24 00:27:43 +0545

    test:user

--- service/user/routes_test.go
+++ service/user/routes_test.go
@@ -19,34 +19,64 @@ func TestUserServiceHandler(t *testing.T) {
 		payload := types.RegisterUserPayLoad{
 			FirstName: "user",
 			LastName:  "joe",
-			Email:     "",
-			Password:  "123",
+			Email:     "invalid",
+			Password:  "password",
 		}
+
 		marshalled, _ := json.Marshal(payload)
+
 		req, err := http.NewRequest(http.MethodPost, "/register", bytes.NewBuffer(marshalled))
 		if err != nil {
 			t.Fatal(err)
 		}
+
 		rr := httptest.NewRecorder()
 		router := mux.NewRouter()
 
-		router.HandleFunc("/register", handler.handlRegister)
+		router.HandleFunc("/register", handler.handleRegister)
 		router.ServeHTTP(rr, req)
+
 		if rr.Code != http.StatusBadRequest {
 			t.Errorf("expected status code %d, got %d", http.StatusBadRequest, rr.Code)
 		}
 	})
+  t.Run("should correctly reigster the user", func(t *testing.T) {
+		payload := types.RegisterUserPayLoad{
+			FirstName: "user",
+			LastName:  "joe",
+			Email:     "valid@test.com",
+			Password:  "password",
+		}
+
+		marshalled, _ := json.Marshal(payload)
+
+		req, err := http.NewRequest(http.MethodPost, "/register", bytes.NewBuffer(marshalled))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+		router := mux.NewRouter()
+
+		router.HandleFunc("/register", handler.handleRegister)
+		router.ServeHTTP(rr, req)
+
+		if rr.Code != http.StatusCreated{
+			t.Errorf("expected status code %d, got %d", http.StatusCreated, rr.Code)
+		}
+	})
 }
 
-type mockUserStore struct {
-}
+type mockUserStore struct{}
 
 func (m *mockUserStore) GetUserByEmail(email string) (*types.User, error) {
 	return nil, nil
 }
-func (m *mockUserStore) GetUserByID(id int) (*types.User, error) {
-	return nil, nil
-}
-func (m *mockUserStore) CreateUser(user types.User) error {
+
+func (m *mockUserStore) CreateUser(u types.User) error {
 	return nil
+}
+
+func (m *mockUserStore) GetUserByID(id int) (*types.User, error) {
+	return &types.User{}, nil
 }
