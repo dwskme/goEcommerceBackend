fa380e9904dc437c4f7c7c8d459b54281c1772a9
dwskme <officialsamirkc@gmail.com>
2024-03-25 18:39:25 +0545

    feat: prodcut and order api

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/dwskme/goEcommerceBackend/service/cart"
+	"github.com/dwskme/goEcommerceBackend/service/order"
 	"github.com/dwskme/goEcommerceBackend/service/product"
 	"github.com/dwskme/goEcommerceBackend/service/user"
 	"github.com/gorilla/mux"
@@ -32,6 +34,13 @@ func (s *APIServer) Run() error {
 	productStore := product.NewStore(s.db)
 	prodcutHandler := product.NewHandler(productStore, userStore)
 	prodcutHandler.RegisterRoutes(subrouter)
+
+	orderStore := order.NewStore(s.db)
+
+	cartHandler := cart.NewHandler(productStore, orderStore, userStore)
+	cartHandler.RegisterRoutes(subrouter)
+
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir("static")))
 
 	log.Println("Listening on:", s.addr)
 	return http.ListenAndServe(s.addr, router)
